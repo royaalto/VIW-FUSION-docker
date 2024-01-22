ed002422783e7e16983ba3efa4e58f6274ac24ab
Yi Zhang <roy6669049@gmail.com>
2024-01-22 12:10:08 +0800

    Fix: running

---
+++ Read.md
@@ -0,0 +1,4 @@
+1.git clone 
+2.put it into wvio parallel to docker
+3../docker_build_melodic.sh
+4.docker_run
--- docker_run.sh
+++ docker_run.sh
@@ -6,6 +6,7 @@ default_name="wiv-fusion"
 container_name=`docker container ls -a | grep $default_name | awk '{print $NF}'`
 VINS_FUSION_DIR="/home/roy/softwares/wheel-vins/VIW-Fusion/"
 KITTI_DATASET="/home/roy/softwares/wheel-vins/bags/"
+rviz -d ../config/vins_rviz_config.rviz
 if [ "$container_name" != "" ]; then
   echo "enter exit container: $container_name"
   docker exec -it $container_name bash
@@ -14,7 +15,8 @@ else
 	docker run \
 	-e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	--rm \
+	-e GDK_SCALE \
+	-e GDK_DPI_SCALE \
 	--net=host \
 	--name $default_name \
 	-v ${VINS_FUSION_DIR}:/root/catkin_ws/src/VIW-Fusion/ \
